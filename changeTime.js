973751c4e27754b29fbe1ccbcf4a4bc4928b0873
Dhriti Roy-UW Bothell Student <68128629+DhritiRoy1@users.noreply.github.com>
2024-10-22 15:38:08 -0700

    Add files via upload

---
+++ changeTime.js
@@ -0,0 +1,25 @@
+var FifteenButton = document.getElementById("15-button");
+var twentyfivemintimer = document.getElementById("25-min");
+var thirtybutton = document.getElementById("30-button");
+var fourtyfivebutton = document.getElementById("45-button");
+var sixtybutton = document.getElementById("60-button");
+FifteenButton.addEventListener('click',()=>{
+    console.log(15)
+    twentyfivemintimer.textContent = FifteenButton.textContent;
+
+})
+thirtybutton.addEventListener('click',()=>{
+    console.log(15)
+    twentyfivemintimer.textContent = thirtybutton.textContent;
+
+})
+fourtyfivebutton.addEventListener('click',()=>{
+    console.log(15)
+    twentyfivemintimer.textContent = fourtyfivebutton.textContent;
+
+})
+sixtybutton.addEventListener('click',()=>{
+    console.log(15)
+    twentyfivemintimer.textContent = sixtybutton.textContent;
+
+})
